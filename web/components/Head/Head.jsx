ecc4b7352933220d2c0dc73cbc796239d2af3368
muhodev <hi@muho.dev>
2022-02-05 15:17:28 +0300

    Update gtag

--- web/components/Head/Head.jsx
+++ web/components/Head/Head.jsx
@@ -5,17 +5,6 @@ export function Head(props) {
   return (
     <NextHead>
       <title>{props.title || ""}</title>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-6BG3M8G3SV"
-      ></Script>
-      <Script>
-        {`  window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-6BG3M8G3SV');`}
-      </Script>
     </NextHead>
   );
 }
--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,8 +1,24 @@
 import { appWithTranslation } from "next-i18next";
+import Script from "next/script";
 import "../styles/main.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-6BG3M8G3SV"
+      ></Script>
+      <Script strategy="lazyOnload">
+        {`  window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-6BG3M8G3SV');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default appWithTranslation(MyApp);
